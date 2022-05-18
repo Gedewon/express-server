61befdabbadd3f1bc8a8e9ad53751c3fef230061
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-19 01:33:00 +0300

    create validator middleware

---
+++ src/middleware/validateRequest.ts
@@ -0,0 +1,31 @@
+import { AnySchema } from "yup";
+import {Request, Response ,NextFunction} from "express";
+import log from "../logger";
+
+export default  (schema: AnySchema)=>async (
+    req:Request, 
+    res:Response,
+    next:NextFunction
+    )=>{
+
+        try{
+            await schema.validate({
+                body: req.body,
+                query: req.query,
+                params: req.params
+            });
+
+            return next();
+        }catch(error){
+            log.error(error);
+            return res.status(400).send(error);
+        }
+
+
+
+
+
+
+
+
+    }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Express, Request, Response } from "express";
-
+import { createUserHandler } from "./controller/user.controller";
+import validateRequest from './middleware/validateRequest'
 export default (app: Express) => {
   app.get("/helthcheck", (req: Request, res: Response) => {
     res.status(200).send("working  correctly");
   });
 
   //create a user
-  app.post("/user");
+  app.post("/api/user",validateRequest(createuserSchema),createUserHandler);
 };
