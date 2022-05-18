0516136b69a56d63f6b8db1ca1d28aa1051c0f77
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-18 16:01:59 +0300

    hash password presave

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -22,6 +22,23 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// use presave hook for model which is cool to hash at flaye  
+UserSchema.pre("save",async function(next){
+    let user = this as UserDocument;
+    // only hash the password if it's hass been modified (or is new)
+    if(!user.isModified("password")) return next();
+
+
+    // Random additional data 
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hashSync(user.password as string,salt);
+
+    // Replace user password with hased one 
+     user.password = hash;
+
+    return next();
+})
+
 UserSchema.methods.comparePassword = async function (candidatePassword : string){
     const user = this as UserDocument;
     return bcrypt.compare(candidatePassword,user.password as string).catch((e)=>false);
