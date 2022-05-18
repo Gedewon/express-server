3890163c6b94b889d98895a8bfcae48efc8058f6
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-18 03:25:04 +0300

    express up and running

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('hie');
+import express from "express";
+import config from "../config/default";
+
+const {port ,host ,dbUri} = config;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+app.listen(port,host,()=>{
+    console.log(`Server listing at http://${host}:${port}`);
+})
