e6e9ec37e3332a38896031f3baaf1341756686c0
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-18 03:37:02 +0300

    Please use logger insted of console.log

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import config from "../config/default";
+import log from "./logger";
 
 const {port ,host ,dbUri} = config;
 const app = express();
@@ -9,5 +10,7 @@ app.use(express.urlencoded({extended: false}));
 
 
 app.listen(port,host,()=>{
-    console.log(`Server listing at http://${host}:${port}`);
+    // since node js is single traded and console.log will block IO service so
+    // try using logger's as much as possible 
+    log.info(`Server listing at http://${host}:${port}`);
 })
