15633c2e9c3bd1f710c4df2deafcd67848bf7c11
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-19 22:52:53 +0300

    creaete session from header aggent

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,7 @@
 import log from "../logger";
 import { Request,Response } from "express";
 import { validatePassword } from "../service/user.service";
+import { createSession } from "../service/session.service";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
     
@@ -16,6 +17,7 @@ export async function createUserSessionHandler(req: Request, res: Response) {
     if(!user) return res.status(401).send("Invalid username or password");
     
   //  now we can create a session 
+     const session = await createSession(user._id, req.get("user-agent") || "");
     
 
 
