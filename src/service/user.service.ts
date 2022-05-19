b20f83452a33043af85378b7ae58268747170f32
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-19 22:38:17 +0300

    implement validate password functionality

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { omit } from "lodash";
 import { DocumentDefinition } from "mongoose";
 import User, { UserDocument } from "../model/user.model";
 
@@ -8,4 +9,21 @@ export async function createUser(input: DocumentDefinition<UserDocument>) {
   } catch (error) {
     throw new Error(error as string);
   }
+}
+
+export async function validatePassword({
+  email,
+  password
+}:{
+  email:UserDocument["email"],
+  password:string,
+}){
+  const user = await User.findOne({email});
+  if(!user) return false;
+
+  
+  const isValid = await user.comparePassword(password);
+  if(!isValid) return false;
+
+  return omit(user.toJSON(),"password")
 }
