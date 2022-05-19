4e7f2af7e818994a00c24e6ac1627b09fceb96a9
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-20 00:39:06 +0300

    Add check health check status

--- src/routes.ts
+++ src/routes.ts
@@ -10,11 +10,16 @@ import { createUserSchema } from "./schema/user.schema";
 import { createUserSessionSchema } from "./schema/session.schema";
 
 export default (app: Express) => {
+  app.get("/helthcheck", (req:Request, res:Response)=>{
+    res.status(200).send({"message":"Server up and running "})
+  });
   app.get("/api/users", getUsersHandler);
   app.get("/api/users/:id", getUserHandler);
 
   //create a user
-  app.post("/api/user", validateRequest(createUserSchema), createUserHandler);
+  app.post("/api/user", 
+  validateRequest(createUserSchema), 
+  createUserHandler);
 
   // Login
   app.post(
