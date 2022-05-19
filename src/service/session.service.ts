f3445d08a361acb4efbd857607da272a030e875e
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-19 22:50:48 +0300

    parse it JSON

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -2,5 +2,5 @@ import Session from "../model/session.model";
 
 export async function createSession(userId: string ,userAgent:string){
      const session = await Session.create({user:userId,userAgent:userAgent})
-    return session;
+    return session.toJSON();
 }
