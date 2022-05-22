56d74fd7699bc960a7bf4f68bb7784bdd76ce09f
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-22 17:22:36 +0300

    Add deseralize user functionality

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import config from "../config/default";
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
 const { port, host, dbUri } = config;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(deserializeUser)
+
+
 
 app.listen(port, host, () => {
   // since node js is single traded and console.log will block IO service so
