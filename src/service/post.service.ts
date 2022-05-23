e13e4e98c204505fe21c885f1c57d133063025a6
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-23 15:54:41 +0300

    create post  update service

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -9,3 +9,18 @@ import Post, { PostDocument } from "../model/post.model";
 export function createPost(input: DocumentDefinition<PostDocument>) {
   return Post.create(input);
 }
+
+export function findPost(
+  query: FilterQuery<PostDocument>,
+  options: QueryOptions = { lean: true }
+) {
+  return Post.findOne(query, {}, options);
+}
+
+export function findAndUpdate(
+  query: FilterQuery<PostDocument>,
+  update: UpdateQuery<PostDocument>,
+  options: QueryOptions
+) {
+  return Post.findOneAndUpdate(query, update, options);
+}
